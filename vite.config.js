e1d09f035bd7943dea32974f97956d0baac9ac0f
Anna Kyshtymova <annakyshtymova@MacBook-Air-Anna.local>
2026-03-12 00:57:13 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/iron-workout/',  // название твоего репозитория
+  base: '/iron_workout/',  // название твоего репозитория
   plugins: [react()],
 })
